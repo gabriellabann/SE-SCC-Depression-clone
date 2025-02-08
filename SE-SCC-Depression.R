f7ef6c5fab0a1028f64209bc670b3e89b7219c0c
Gabriella <gabriellabann@uchicago.edu>
2025-02-08 16:37:21 -0600

    Made notes on what packadges were installed

---
+++ SE-SCC-Depression.R
@@ -0,0 +1,6 @@
+# ran install.packages("quarto") in console
+# ran library(quarto) in console
+
+# verified getwd() in console
+
+# ran quarto::quarto_use_template("wjschne/apaquarto", no_prompt = T) in console
