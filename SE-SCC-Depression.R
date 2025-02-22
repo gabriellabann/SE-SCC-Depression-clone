ddc878ba8c6e0e06bab413b28674cf98a6b0e3c3
Gabriella <gabriellabann@uchicago.edu>
2025-02-21 22:32:48 -0600

    installed packadges, noted so

--- SE-SCC-Depression.R
+++ SE-SCC-Depression.R
@@ -4,3 +4,36 @@
 # verified getwd() in console
 
 # ran quarto::quarto_use_template("wjschne/apaquarto", no_prompt = T) in console
+
+# ran install.packages("tidyverse") in console
+# ran library(tidyverse) in console
+
+# ran install.packages("scales") in console
+# ran library(scales) in console
+
+# ran install.packages("psych") in console
+# ran library(psych) in console
+
+# ran install.packages("stats") in console
+# ran library(stats) in console
+
+# ran install.packages("lme4") in console
+# ran library(lme4) in console
+
+# ran install.packages("patchwork") in console
+# ran library(patchwork) in console
+
+# ran install.packages("kableExtra") in console
+# ran library(kableExtra) in console
+
+# ran install.packages("rempsyc") in console
+# ran library(rempsyc) in console
+
+# ran install.packages("broom") in console
+# ran library(broom) in console
+
+# ran install.packages("papaja") in console
+# ran library(papaja) in console
+
+# ran install.packages("flextable") in console
+# ran library(flextable) in console
