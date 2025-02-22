9749e05d2898fd6409218494c68d0fff557d598b
Gabriella <gabriellabann@uchicago.edu>
2025-02-22 16:45:30 -0600

    began set up for final project (planning on using skeleton repo for final project since set up): set seed, read in data, started cleaning data

--- SE-SCC-Depression.R
+++ SE-SCC-Depression.R
@@ -37,3 +37,32 @@
 
 # ran install.packages("flextable") in console
 # ran library(flextable) in console
+
+#| label: setup
+
+library(tidyverse)
+set.seed(1:10000, 1)
+
+#| label: reading in data
+
+library(readr)
+setwd("C:/Users/gabri/repos/SE-SCC-Depression/LocalOnly")
+wave1_cohort1 <- readr::read_csv("Wave1_Cohort1.csv",
+                          trim_ws = FALSE, 
+                          name_repair = "minimal", 
+                          col_types = cols(.default = col_character()))
+#| label: inspecting the data
+
+head(wave1_cohort1)
+str(wave1_cohort1)
+glimpse(wave1_cohort1)
+
+#| label: cleaning the data
+
+wave1_cohort1_clean <- wave1_cohort1
+
+colnames(wave1_cohort1)
+  
+wave1_cohort1_clean <- wave1_cohort1_clean %>%
+  select(RosenbergQ1 = `Rosenberg Q1_1`, RosenbergQ2 = `Rosenberg Q1_2`, RosenbergQ3 = `Rosenberg Q1_3`, RosenbergQ4 = `Rosenberg Q1_4`, RosenbergQ5 = `Rosenberg Q1_5`, RosenbergQ6 = `Rosenberg Q1_6`, RosenbergQ7 = `Rosenberg Q1_7`, RosenbergQ8 = `Rosenberg Q1_8`, RosenbergQ9 = `Rosenberg Q1_9`, RosenbergQ10 = `Rosenberg Q1_10`)
+# make sure to also clean self-IAT, SCC, and CES-D
