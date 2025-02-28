11b80eb0dc608134b59d36c92408f515c201f1a4
Gabriella <gabriellabann@uchicago.edu>
2025-02-28 15:15:20 -0600

    debugging cleaning

--- SE-SCC-Depression.R
+++ SE-SCC-Depression.R
@@ -1,26 +1,26 @@
 #| label: loading required libraries
 
 # ran quarto::quarto_use_template("wjschne/apaquarto", no_prompt = T) in console
-if (!requireNamespace("quarto", quietly = TRUE)) install.packages("tidyverse")
-if(!requireNamespace("tidyverse", quietly = TRUE)) install.packages("tidyverse")
-if (!requireNamespace("readr", quietly = TRUE)) install.packages("readr")
-if (!requireNamespace("forcats", quietly = TRUE)) install.packages("forcats")
-if (!requireNamespace("ggplot2", quietly = TRUE)) install.packages("ggplot2")
-if (!requireNamespace("quarto", quietly = TRUE)) install.packages("quarto")
-if (!requireNamespace("knitr", quietly = TRUE)) install.packages("knitr")
-if (!requireNamespace("stringr", quietly = TRUE)) install.packages("stringr")
-if (!requireNamespace("dplyr", quietly = TRUE)) install.packages("dplyr")
-if (!requireNamespace("tidyr", quietly = TRUE)) install.packages("tidyr")
-if(!requireNamespace("scales", quietly = TRUE)) install.packages("scales")
-if (!requireNamespace("psych", quietly = TRUE)) install.packages("psych")
-if (!requireNamespace("stats", quietly = TRUE)) install.packages("stats")
-if (!requireNamespace("lme4", quietly = TRUE)) install.packages("lme4")
-if (!requireNamespace("patchwork", quietly = TRUE)) install.packages("patchwork")
-if (!requireNamespace("kableExtra", quietly = TRUE)) install.packages("kableExtra")
-if (!requireNamespace("rempsyc", quietly = TRUE)) install.packages("rempsyc")
-if (!requireNamespace("broom", quietly = TRUE)) install.packages("broom")
-if (!requireNamespace("papaja", quietly = TRUE)) install.packages("papaja")
-if (!requireNamespace("flextable", quietly = TRUE))install.packages("flextable")
+#if (!requireNamespace("quarto", quietly = TRUE)) install.packages("tidyverse")
+#if(!requireNamespace("tidyverse", quietly = TRUE)) install.packages("tidyverse")
+#if (!requireNamespace("readr", quietly = TRUE)) install.packages("readr")
+#if (!requireNamespace("forcats", quietly = TRUE)) install.packages("forcats")
+#if (!requireNamespace("ggplot2", quietly = TRUE)) install.packages("ggplot2")
+#if (!requireNamespace("quarto", quietly = TRUE)) install.packages("quarto")
+#if (!requireNamespace("knitr", quietly = TRUE)) install.packages("knitr")
+#if (!requireNamespace("stringr", quietly = TRUE)) install.packages("stringr")
+#if (!requireNamespace("dplyr", quietly = TRUE)) install.packages("dplyr")
+#if (!requireNamespace("tidyr", quietly = TRUE)) install.packages("tidyr")
+#if(!requireNamespace("scales", quietly = TRUE)) install.packages("scales")
+#if (!requireNamespace("psych", quietly = TRUE)) install.packages("psych")
+#if (!requireNamespace("stats", quietly = TRUE)) install.packages("stats")
+#if (!requireNamespace("lme4", quietly = TRUE)) install.packages("lme4")
+#if (!requireNamespace("patchwork", quietly = TRUE)) install.packages("patchwork")
+#if (!requireNamespace("kableExtra", quietly = TRUE)) install.packages("kableExtra")
+#if (!requireNamespace("rempsyc", quietly = TRUE)) install.packages("rempsyc")
+#if (!requireNamespace("broom", quietly = TRUE)) install.packages("broom")
+#if (!requireNamespace("papaja", quietly = TRUE)) install.packages("papaja")
+#if (!requireNamespace("flextable", quietly = TRUE))install.packages("flextable")
 
 library(quarto)
 library(tidyverse)
@@ -69,7 +69,7 @@ glimpse(wave1_cohort1)
 
 #| label: cleaning the data
 
-#wave1_cohort1_clean <- wave1_cohort1 %>%
+wave1_cohort1_clean <- wave1_cohort1 #%>%
   #rename_with(~ str_replace_all(., " ", "_")) %>%
   #mutate(across(everything(), trimws))
 
@@ -78,13 +78,11 @@ colnames(wave1_cohort1)
 colnames(wave1_cohort1_clean)
   
 wave1_cohort1_clean <- wave1_cohort1_clean %>%
-  rename(RosenbergQ1 = `Rosenberg_Q1_1`, RosenbergQ2 = `Rosenberg_Q1_2`, 
-         RosenbergQ3 = `Rosenberg_Q1_3`, RosenbergQ4 = `Rosenberg_Q1_4`, 
-         RosenbergQ5 = `Rosenberg_Q1_5`, RosenbergQ6 = `Rosenberg_Q1_6`, 
-         RosenbergQ7 = `Rosenberg_Q1_7`, RosenbergQ8 = `Rosenberg_Q1_8`, 
-         RosenbergQ9 = `Rosenberg_Q1_9`, RosenbergQ10 = `Rosenberg_Q1_10`)
-
-wave1_cohort1_clean <- wave1_cohort1 %>%
+  rename(RosenbergQ1 = `Rosenberg Q1_1`, RosenbergQ2 = `Rosenberg Q1_2`, 
+         RosenbergQ3 = `Rosenberg Q1_3`, RosenbergQ4 = `Rosenberg Q1_4`, 
+         RosenbergQ5 = `Rosenberg Q1_5`, RosenbergQ6 = `Rosenberg Q1_6`, 
+         RosenbergQ7 = `Rosenberg Q1_7`, RosenbergQ8 = `Rosenberg Q1_8`, 
+         RosenbergQ9 = `Rosenberg Q1_9`, RosenbergQ10 = `Rosenberg Q1_10`) %>%
   rename(CESDQ1 = `CES-D Q1_1`, CESDQ2 = `CES-D Q1_2`, CESDQ3 = `CES-D Q1_3`, 
          CESDQ4 = `CES-D Q1_4`, CESDQ5 = `CES-D Q1_5`, CESDQ6 = `CES-D Q1_6`, 
          CESDQ7 = `CES-D Q1_7`, CESDQ8 = `CES-D Q1_8`, CESDQ9 = `CES-D Q1_9`, 
@@ -95,15 +93,17 @@ wave1_cohort1_clean <- wave1_cohort1 %>%
 #make sure to also clean SCC
 #SCC was forgotten for cohort 1, it is included in wave 2, wave 3, and will be included in subsequent cohorts of wave 1
 
-for (col in colnames(wave1_cohort1_clean)) {
-  if (any(is.na(wave1_cohort1_clean[[col]]))) {
-    print(paste("Column", col, "has missing values"))
-  }}
+#for (col in colnames(wave1_cohort1_clean)) {
+  #if (any(is.na(wave1_cohort1_clean[[col]]))) {
+    #print(paste("Column", col, "has missing values"))
+  #}}
 
-wave1_cohort1_clean <- wave1_cohort1_clean %>%
-  pivot_longer(cols = starts_with("CESDQ"), names_to = "Question", values_to = "Response") %>%
-  group_by(Question) %>%
-  summarise(Mean_Response = mean(Response, na.rm = TRUE))
+#drop_na(RosenbergQ1) #do this within the pipeline of each questionnaire if there are nas to deal with
+
+#wave1_cohort1_clean <- wave1_cohort1_clean %>%
+  #pivot_longer(cols = starts_with("CESDQ"), names_to = "Question", values_to = "Response") %>%
+  #group_by(Question) %>%
+  #summarise(Mean_Response = mean(Response, na.rm = TRUE))
 
 #| label: cleaning and scoring self-IAT
 
@@ -112,21 +112,21 @@ install.packages("remotes")
 remotes::install_github("iatgen/iatgen", force = TRUE)
 library(iatgen)
 
-dat <- read.csv("Wave1_Cohort1_SelfIAT.csv", header=T)
+wave1_cohort1_IAT <- read.csv("Wave1_Cohort1_SelfIAT.csv", header=T)
 
 #collapsing IAT data down
-dat$compatible.crit <- combineIATfourblocks(dat$Q4.RP4, dat$Q18.LP4, dat$Q14.RN7, dat$Q28.LN7)
-dat$incompatible.crit <- combineIATfourblocks(dat$Q7.RP7, dat$Q21.LP7, dat$Q11.RN4, dat$Q25.LN4)
+wave1_cohort1_IAT$compatible.crit <- combineIATfourblocks(wave1_cohort1_IAT$Q4.RP4, wave1_cohort1_IAT$Q18.LP4, wave1_cohort1_IAT$Q14.RN7, wave1_cohort1_IAT$Q28.LN7)
+wave1_cohort1_IAT$incompatible.crit <- combineIATfourblocks(wave1_cohort1_IAT$Q7.RP7, wave1_cohort1_IAT$Q21.LP7, wave1_cohort1_IAT$Q11.RN4, wave1_cohort1_IAT$Q25.LN4)
 
 #collapsing IAT practice blocks
-dat$compatible.prac<- combineIATfourblocks(dat$Q3.RP3, dat$Q17.LP3, dat$Q13.RN6, dat$Q27.LN6)
-dat$incompatible.prac <- combineIATfourblocks(dat$Q6.RP6, dat$Q20.LP6, dat$Q10.RN3, dat$Q24.LN3)
+wave1_cohort1_IAT$compatible.prac<- combineIATfourblocks(wave1_cohort1_IAT$Q3.RP3, wave1_cohort1_IAT$Q17.LP3, wave1_cohort1_IAT$Q13.RN6, wave1_cohort1_IAT$Q27.LN6)
+wave1_cohort1_IAT$incompatible.prac <- combineIATfourblocks(wave1_cohort1_IAT$Q6.RP6, wave1_cohort1_IAT$Q20.LP6, wave1_cohort1_IAT$Q10.RN3, wave1_cohort1_IAT$Q24.LN3)
 
 #cleaning the IAT
-clean <- cleanIAT(prac1=dat$compatible.prac, 
-                  crit1=dat$compatible.crit, 
-                  prac2=dat$incompatible.prac, 
-                  crit2=dat$incompatible.crit, 
+clean <- cleanIAT(prac1=wave1_cohort1_IAT$compatible.prac, 
+                  crit1=wave1_cohort1_IAT$compatible.crit, 
+                  prac2=wave1_cohort1_IAT$incompatible.prac, 
+                  crit2=wave1_cohort1_IAT$incompatible.crit, 
                   
                   timeout.drop=TRUE, 
                   timeout.ms=10000, 
@@ -155,7 +155,7 @@ names(clean)
 IATreliability(clean)$reliability #internal consistency (0.8902007)
 IATalpha(clean)$alpha.total #reliability of IAT D-score (raw_alpha = 0.7397427)
 
-dat$D <- clean$D
+wave1_cohort1_IAT$D <- clean$D
 mean(clean$D, na.rm=T) #(0.7055081)
 sd(clean$D, na.rm=T) #(0/3755495)
 t.test(clean$D) #one sample t-test (t = 7.0291, df = 13, p-value = 8.945e-06, CI(0.4886723, 0.9223440), mean of x = 0.7055081)
@@ -169,16 +169,16 @@ s_iat_cohens_d <- function(x, y) {
   pooled_sd <- sqrt((sd(x, na.rm = TRUE)^2 + sd(y, na.rm = TRUE)^2) / 2)
   return(mean_diff / pooled_sd)}
 
-d_value <- cohens_d(clean$D, rep(0, length(clean$D)))
+d_value <- s_iat_cohens_d(clean$D, rep(0, length(clean$D)))
 
 # plotting IAT data
 
 library(ggplot2)
-ggplot(dat, aes(x=D))+
+ggplot(wave1_cohort1_IAT, aes(x=D))+
   geom_density(color="black", fill="light blue")+
   theme_light()
 
-write_csv(clean$D, "SelfIATOutput.csv")
+write.csv(clean$D, "SelfIATOutput.csv")
 
 #reaction time descriptives by block
 mean(clean$clean.means.crit1, na.rm=T) #(1028.404)
@@ -212,12 +212,17 @@ wave1_cohort1_clean <- wave1_cohort1_clean %>%
 library(tidyverse)
 
 #second option converting CES-D responses using forcats
+levels(as.factor(wave1_cohort1_clean$CESDQ1)) #1
+
 wave1_cohort1_clean <- wave1_cohort1_clean %>%
-  mutate(across(starts_with("CESDQ"), ~ fct_recode(as.factor(.),
+  mutate(across(starts_with("CESDQ"), ~ str_trim(.)))
+
+wave1_cohort1_clean <- wave1_cohort1_clean %>% #this gives errors, fix!!
+  mutate(across(starts_with("CESDQ"), ~ as.numeric(fct_recode(as.factor(.),
                                                    "0" = "Rarely or none of the time (less than 1 day)",
                                                    "1" = "Some or a little of the time (1-2 days)",
                                                    "2" = "Occasionally or a moderate amount of time (3-4 days)",
-                                                   "3" = "Most or all of the time (5-7 days)")) %>% as.numeric())
+                                                   "3" = "Most or all of the time (5-7 days)")) %>% as.numeric()))
 
 
 #reverse scoring positive items
